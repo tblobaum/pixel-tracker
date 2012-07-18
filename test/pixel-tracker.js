9b4cbbde7df99a81a294b275f5957e0b39dcdcb5
Thomas Blobaum <tblobaum@gmail.com>
2012-07-18 03:10:09 -0500

    update test for formatting, app.close

--- test/pixel-tracker.js
+++ test/pixel-tracker.js
@@ -1,44 +1,44 @@
-var express = require('express');
-var test = require("tap").test
-var pixelTracker = require("../") //Load the parent directory, looks for index.js 
-var http = require('http')
+var express = require('express')
+  , test = require('tap').test
+  , tracker = require('../')
 
-test("testing pixel tracker",function(t){
+test('testing pixel tracker', function (t) {
 
-  var app = module.exports = express.createServer();
+  var app = express.createServer()
 
-  app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
-  });
+  app.configure(function () {
+    app.use(express.cookieParser())
+  })
 
-  pixelTracker
-    .use(function(e,res){
-      t.type(res.decay,'number','Decay is a number')
-      t.type(res.cookies._tracker,'string','_tracker should be created for all request')
-      t.notOk(res.ua,'For testing, user agent browser should be false')
-      t.type(res.host,'string','host should valid')
-      t.type(res.domain,'string','domain should valid')
-      t.type(res.geo.ip,'string','ip should valid')
+  tracker
+    .use(function (e, res) {
+      t.type(res.decay, 'number', 'Decay is a number')
+      t.type(res.cookies._tracker, 'string','_tracker should be created for all request')
+      t.notOk(res.ua, 'For testing, user agent browser should be false')
+      t.type(res.host, 'string', 'host should valid')
+      t.type(res.domain, 'string', 'domain should valid')
+      t.type(res.geo.ip, 'string', 'ip should valid')
     })
-    .use(function(e,res){
-      t.type(res.decay,'number','Decay is a number')
-      t.type(res.cookies._tracker,'string','_tracker should be created for all request')
-      t.notOk(res.ua,'For testing, user agent browser should be false')
-      t.type(res.host,'string','host should valid')
-      t.type(res.domain,'string','domain should valid')
-      t.type(res.geo.ip,'string','ip should valid')
+    .use(function (e, res) {
+      t.type(res.decay, 'number', 'Decay is a number')
+      t.type(res.cookies._tracker, 'string', '_tracker should be created for all request')
+      t.notOk(res.ua, 'For testing, user agent browser should be false')
+      t.type(res.host, 'string', 'host should valid')
+      t.type(res.domain, 'string', 'domain should valid')
+      t.type(res.geo.ip, 'string', 'ip should valid')
       t.end()
     })
 
-  app.all('/pixel',pixelTracker.middleware)
-  app.listen(3000);
+  app.all('/pixel', tracker.middleware)
+  app.listen(3000)
 
-  http.get({host:'localhost',port:3000,path:'/pixel',agent:false},function(res){
-    // Stop the server
-    process.exit(0)
+  require('http').get({ 
+      host : 'localhost'
+    , port : 3000
+    , path : '/pixel'
+    , agent : false
+  }, function (res) {
+    app.close()
   })
+
 })
